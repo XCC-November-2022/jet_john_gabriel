8195add8b74ff9d3bf7c06f63b54d1ecfe00f3e8
gaburierus <steleagabi@gmail.com>
2022-11-07 17:42:05 +0200

    test

---
+++ src/jet/commands.py
@@ -0,0 +1,18 @@
+import subprocess
+
+
+class Commands:
+
+    def __init__(self):
+        self.git = None
+
+    def commit(self):
+        pass
+
+    def commit_uncommitted(self):
+        pass
+
+    def resolve_conflicts(self):
+        pass
+
+
